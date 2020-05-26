ad75d20966a4f0eb64bd5b8c67ea8d10e85cb54c
Ali <alifelanv@gmail.com>
2020-05-26 13:19:59 -0500

    Improve code, remove unnecessary complexity

--- main.c
+++ main.c
@@ -5,25 +5,7 @@
 //funcion que regresa numero random
 int getRandomNumber(int inf, int sup){
     //Caculamos el tamanio que necesitamos en nuestro arreglo
-    int tamArreglo=sup-inf+1;
-    int numeros[tamArreglo];
-    int unixTime=(int)time(NULL);
-    int index=0;
-   // printf("tam: %d",tamArreglo);
-    //Ciclo for para recorrer
-    for(int i=0; i<tamArreglo;i++){
-        numeros[i]=inf;
-        inf++;
-    }
-    //recorrer el arreglo para saber cuando regresarlo
-    for(int i=0; i<unixTime/3; i++){
-        index++;
-        if(index>=tamArreglo){
-            index=0;
-        }
-
-    }
-    return numeros[index];
+    return inf + (int)time(NULL) % sup-inf+1;
 }
 
 int main()
@@ -32,8 +14,6 @@ int main()
     int inf;
     int sup;
     int resultado;
-    int unixTime=(int)time(NULL);
-    //printf("UnixTime: %d \n",unixTime);
     //preuntar por el numero
     printf("Limite Inferior:\n");
     scanf("%d",&inf);
